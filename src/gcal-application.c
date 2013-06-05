3941693236957cc04ae7c9386de765441a2a6511
Erick Pérez Castellanos <erick.red@gmail.com>
2013-06-04 22:39:02 -0400

    ui: integrated GcalDayView in GcalWindow
    Initial work. Added option to GcalApplication action.

--- src/gcal-application.c
+++ src/gcal-application.c
@@ -296,9 +296,10 @@ gcal_application_set_app_menu (GApplication *app)
   g_action_map_add_action ( G_ACTION_MAP (app), G_ACTION (priv->view));
 
   view_as = g_menu_new ();
-  g_menu_append (view_as, _("Weeks"), "app.view::week");
-  g_menu_append (view_as, _("Months"), "app.view::month");
-  g_menu_append (view_as, _("Years"), "app.view::year");
+  g_menu_append (view_as, _("Day"), "app.view::day");
+  g_menu_append (view_as, _("Week"), "app.view::week");
+  g_menu_append (view_as, _("Month"), "app.view::month");
+  g_menu_append (view_as, _("Year"), "app.view::year");
 
   g_menu_append_section (app_menu, _("View as"), G_MENU_MODEL (view_as));
 
