079adfb7a0da4d43516790993f1f0c9c9701683a
Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
2017-02-20 08:27:48 -0300

    window: reset edit dialog's event on response
    Currently, we keep the to-be-edited event of the Edit
    dialog when it sends any response. However, this can
    cause issues since we can edit the event, press Esc and
    see that the event didn't change but Edit dialog keeps
    the changes.
    
    Fix that by always resetting the edited event to NULL
    after a response.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=778644

--- src/gcal-window.c
+++ src/gcal-window.c
@@ -1102,6 +1102,7 @@ edit_dialog_closed (GtkDialog *dialog,
   event = gcal_edit_dialog_get_event (edit_dialog);
   view = GCAL_VIEW (window->views[window->active_view]);
 
+  gcal_edit_dialog_set_event (edit_dialog, NULL);
   gtk_widget_hide (GTK_WIDGET (dialog));
 
   switch (response)
