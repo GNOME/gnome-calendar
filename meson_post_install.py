4cb0b1434347bd585f64d696f2a90061acf8ea70
Iñigo Martínez <inigomartinez@gmail.com>
2017-07-18 18:15:14 +0200

    project: Renamed the names of the icons' files
    The names of icons' files, besides containing the final names, they
    also contained the path in which they were going to be installed.
    This implies that they have to be renamed on the installation
    process.
    
    This patch creates the proper directory tree needed on the
    installation process and renames all files' names to their final
    names.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=782843

--- meson_post_install.py
+++ meson_post_install.py
@@ -1,19 +1,10 @@
 #!/usr/bin/env python3
 
-import glob
 import os
-import re
 import subprocess
 
 install_prefix = os.environ['MESON_INSTALL_PREFIX']
-
-name_pattern = re.compile('hicolor_apps_(?:\d+x\d+|symbolic)_(.*)')
-search_pattern = '/**/hicolor_*'
-
 icondir = os.path.join(install_prefix, 'share', 'icons', 'hicolor')
-[os.rename(file, os.path.join(os.path.dirname(file), name_pattern.search(file).group(1)))
- for file in glob.glob(icondir + search_pattern, recursive=True)]
-
 schemadir = os.path.join(install_prefix, 'share', 'glib-2.0', 'schemas')
 
 if not os.environ.get('DESTDIR'):
