7d869ed4e8c924173c35c80b1079a1d8e13a414d
Federico Mena Quintero <federico@gnome.org>
2025-05-26 19:44:10 -0600

    CI: copy the scanbuild directory from the correct place
    Ninja's scan-build command creates a directory with an ugly unique
    name.  So, we copy it to a well-known "scanbuild" directory instead.
    
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-calendar/-/merge_requests/580>

--- .gitlab-ci/build-static-scan.sh
+++ .gitlab-ci/build-static-scan.sh
@@ -5,4 +5,7 @@ set -eux -o pipefail
 meson setup --buildtype=debug  --prefix /usr _scan_build .
 ninja -C _scan_build scan-build
 mkdir -p public/
-mv _scan_build/meson-logs/scanbuild public/scanbuild
+
+# The scan-build command creates _scan_build/meson-logs/scanbuild/UUID which is ugly,
+# so we copy it to a friendly name instead.
+cp -r _scan_build/meson-logs/scanbuild/* public/scanbuild
