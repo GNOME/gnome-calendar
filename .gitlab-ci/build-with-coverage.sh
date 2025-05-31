20b6f92366ecedef5c1d42e2c217d15c638c69fd
Jordan Petridis <jpetridis@gnome.org>
2025-05-31 06:56:07 +0300

    ci: Replace scanbuild and coverage jobs with the template
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-calendar/-/merge_requests/584>

--- .gitlab-ci/build-with-coverage.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -eux -o pipefail
-
-export CFLAGS="-coverage -ftest-coverage -fprofile-arcs"
-
-meson setup --buildtype=debug --prefix /usr _build .
-meson compile -C _build
-meson test -C _build
