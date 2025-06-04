73e1baad04b4737cfd2133404a04819c118f2d3e
Hari Rana <theevilskeleton@riseup.net>
2025-06-04 13:00:38 -0400

    gui/event-widget: Set accessible role as toggle button
    Since it's a toggleable button with a popup, it makes more sense
    to set its accessible role as GTK_ACCESSIBLE_ROLE_TOGGLE_BUTTON.
    
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-calendar/-/merge_requests/587>

--- src/gui/gcal-event-widget.c
+++ src/gui/gcal-event-widget.c
@@ -894,7 +894,7 @@ gcal_event_widget_class_init (GcalEventWidgetClass *klass)
   gtk_widget_class_bind_template_callback (widget_class, on_drag_source_prepare_cb);
 
   gtk_widget_class_set_css_name (widget_class, "event");
-  gtk_widget_class_set_accessible_role (widget_class, GTK_ACCESSIBLE_ROLE_BUTTON);
+  gtk_widget_class_set_accessible_role (widget_class, GTK_ACCESSIBLE_ROLE_TOGGLE_BUTTON);
 
   gtk_widget_class_set_layout_manager_type (widget_class, GTK_TYPE_BOX_LAYOUT);
 }
